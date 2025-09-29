a3fcdb7f0c94cc5e5e2dbb581276922eb1de2b64
riiwayatdev-lang <riiwayatdev@gmail.com>
2025-09-29 10:22:40 +0700

    feat: add logout button

---
+++ lib/services/api_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiService {
+  static const String baseUrl = "http://192.168.110.127:8000/api";
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode == 200 && data['success'] == true) {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('auth_token', data['data']['auth_token']);
+      return data['data'];
+    } else {
+      throw Exception(data['message'] ?? 'Login gagal');
+    }
+  }
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('auth_token');
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('auth_token');
+  }
+}
